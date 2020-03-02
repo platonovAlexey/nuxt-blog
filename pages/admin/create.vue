6f57afd144403794ed4b8a9ce6724610ab4b636d
platonovAlexey <kalyawe4ka@gmail.com>
2020-03-02 23:35:13 +0300

    add create page ui

--- pages/admin/create.vue
+++ pages/admin/create.vue
@@ -1,10 +1,79 @@
 <template>
-  <h1>Create</h1>
+  <el-form 
+    @submit.native.prevent="onSubmit" 
+    :model="controls" 
+    :rules="rules" 
+    ref="form"
+    >
+    <h1 class="mb">Создать новый пост</h1>
+    <el-form-item label="Введите название поста" prop="title">
+      <el-input v-model.trim="controls.title" />
+    </el-form-item>
+    <el-form-item label="Текст в формате .md или .html" prop="text">
+      <el-input v-model.trim="controls.text" type="textarea" resize="none" :rows="10"/>
+    </el-form-item>
+
+    <el-form-item>
+      <el-button 
+        type="primary" 
+        round
+        native-type="submit"
+        :loading="loading"
+        >
+        Создать пост</el-button>
+    </el-form-item>
+  </el-form>
 </template>
 
 <script>
 export default {
   layout: 'admin',
-  middleware: ['admin-auth']
+  middleware: ['admin-auth'],
+  data() {
+    return {
+      loading: false,
+      controls: {
+        text: '',
+        title: ''
+      },
+      rules: {
+        text: [
+          { required: true, message: 'Текст не должен быть пустым', trigger: 'blur' }
+        ],
+        title: [
+          { required: true, message: 'Название поста не должно быть пустым', trigger: 'blur' }
+        ]
+      }
+    }
+  },
+  methods: {
+    onSubmit () {
+      this.$refs.form.validate(async valid => {
+        if (valid) {
+          this.loading = true
+
+          const formData = {
+            text: this.controls.text,
+            title: this.controls.title
+          }
+
+          try {
+            await this.$store.dispatch('post/create', formData)
+            this.controls.text = ''
+            this.controls.title = ''
+            this.$message.success('Пост создан')
+          } catch(e) {} finally {
+            this.loading = false
+          }
+        }
+      })
+    }
+  }
 }
 </script>
+
+<style scoped lang="scss">
+form{
+  width: 600px;
+}
+</style>
