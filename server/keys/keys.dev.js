ec7ad154971a20b55c60b88a29910fb7e73bedb4
platonovAlexey <kalyawe4ka@gmail.com>
2020-03-06 08:43:21 +0300

    fix

--- server/keys/keys.dev.js
+++ server/keys/keys.dev.js
@@ -1,3 +1,3 @@
 module.exports = {
-  MONGO_URI: `mongodb+srv://admin:Platonov210291@cluster0-rpmew.mongodb.net/test?retryWrites=true&w=majority`
+  MONGO_URI: `mongodb+srv://admin:admin@cluster0-rpmew.mongodb.net/test?retryWrites=true&w=majority`
 }
