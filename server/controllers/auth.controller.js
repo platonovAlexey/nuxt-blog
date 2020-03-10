96498637ec953cea9b6fdcfcb002de853b9ff926
platonovAlexey <kalyawe4ka@gmail.com>
2020-03-10 22:44:34 +0300

    add authorization user, connect front or back

--- server/controllers/auth.controller.js
+++ server/controllers/auth.controller.js
@@ -1,5 +1,26 @@
-module.exports.login = (req, res) => {
-  
+const bcrypt = require('bcrypt-nodejs')
+const jwt = require('jsonwebtoken')
+const keys = require('../keys')
+const User = require('../models/user.model')
+
+module.exports.login = async (req, res) => {
+  const candidate = await User.findOne({login: req.body.login})
+
+  if (candidate) {
+    const isPasswodCorrect = bcrypt.compareSync(req.body.password, candidate.password)
+
+    if (isPasswodCorrect) {
+      const token = jwt.sign({
+        login: candidate.login,
+        userId: candidate._id
+      }, keys.JWT, {expiresIn: 60*60})
+      res.json({token})
+    } else {
+      res.status(401).json({message: 'Пароль не верный'})
+    }
+  } else {
+    res.status(404).json({message: 'Пользватель не найден'})
+  }
 }
 
 module.exports.createUser = (req, res) => {
--- server/keys/keys.dev.js
+++ server/keys/keys.dev.js
@@ -1,3 +1,4 @@
 module.exports = {
-  MONGO_URI: `mongodb+srv://admin:admin@cluster0-rpmew.mongodb.net/test?retryWrites=true&w=majority`
+  MONGO_URI: `mongodb+srv://admin:admin@cluster0-rpmew.mongodb.net/test?retryWrites=true&w=majority`,
+  JWT: 'dev-jwt-key'
 }
