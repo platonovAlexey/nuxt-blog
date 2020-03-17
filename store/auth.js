d9ab1b66f68228abe52dc217badb843b2ea87a87
platonovAlexey <kalyawe4ka@gmail.com>
2020-03-18 00:56:48 +0300

    work session, auto login, token save to cookies

--- store/auth.js
+++ store/auth.js
@@ -1,3 +1,7 @@
+import Cookie from 'cookie'
+import Cookies from 'js-cookie'
+import jwtDecode from 'jwt-decode'
+
 export const state = () => ({
   token: null
 })
@@ -34,10 +38,24 @@ export const actions = {
   setToken({commit}, token) {
     this.$axios.setToken(token, 'Bearer')
     commit('setToken', token)
+    Cookies.set('jwt-token', token)
   },
   logout({commit}) {
     this.$axios.setToken(false)
     commit('clearToken')
+    Cookies.remove('jwt-token')
+  },
+  autoLogin({dispatch}) {
+    const cookieStr = process.browser ? document.cookie : this.app.context.req.headers.cookie
+
+    const cookies = Cookie.parse(cookieStr || '') || {}
+    const token = cookies['jwt-token']
+    
+    if (isJWTValid(token)) {
+      dispatch('setToken', token)
+    } else {
+      dispatch('logout')
+    }
   }
 
 }
@@ -45,4 +63,15 @@ export const actions = {
 export const getters = {
   isAuthenticated: state => Boolean(state.token),
   token: state => state.token
+}
+
+function isJWTValid(token) {
+  if (!token) {
+    return false
+  }
+
+  const jwtData = jwtDecode(token) || {}
+  const expires = jwtData.exp || 0
+
+  return (new Date().getTime() / 1000) < expires
 }
